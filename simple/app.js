0cd06b95f04c519d487f7806c57c86fd67552aed
Hani Herbert Ho <hani@xendit.co>
2023-08-15 14:25:20 +0800

    feat: run 3 simple api instances

--- simple/app.js
+++ simple/app.js
@@ -1,1 +1,19 @@
-console.log('Hello World from Simple API!')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.status(200).json(req.body)
+})
+
+app.get('/liveness', (req, res) => {
+  res.status(200).json({alive: true})
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Simple API listening on port ${port}!`)
+})
