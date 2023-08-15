f7a996c7c623fe85ad78f1ff34830cc2877188aa
Hani Herbert Ho <hani@xendit.co>
2023-08-15 14:35:05 +0800

    feat: add routing api without logic

--- routing/app.js
+++ routing/app.js
@@ -1,1 +1,28 @@
-console.log('Hello World from Routing API!')
+const express = require('express')
+const axios = require('axios')
+const app = express()
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+  console.log(req.body)  
+  axios.post('http://localhost:3001/', req.body)
+  .then(response => {
+    console.log(response.data)
+    res.status(200).json(response.data)
+  })
+  .catch(error => {
+    console.error(error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  })
+})
+
+app.get('/liveness', (req, res) => {
+  res.status(200).json({alive: true})
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(3000, () => {
+  console.log(`Simple API listening on port ${port}!`)
+})
